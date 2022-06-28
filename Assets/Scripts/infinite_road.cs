617cdb180ab73c73957fb0c6314997e75c8aee4b
Harry <bamdad.kho@gmail.com>
2022-06-28 17:14:46 +0300

    added 3 segments, halfed the size of the segments, added color to the walls to make them easier to see

--- Assets/Scripts/infinite_road.cs
+++ Assets/Scripts/infinite_road.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 public class infinite_road : MonoBehaviour
 {
     [SerializeField] GameObject current;
-    [SerializeField] GameObject blank_segment;
+
+    [SerializeField] GameObject rightWallSegment;
+    [SerializeField] GameObject middleWallSegment;
+    [SerializeField] GameObject leftWallSegment;
+
     GameObject last;
     int currentNum = 1;
     int farthestPlaced = 2;
+
+    void placeSegment(int position)
+    {
+        int type = Random.Range(1, 4);
+
+        if (type == 1)
+        {
+            Instantiate(rightWallSegment, new Vector3 (0, 0, farthestPlaced * 50 + 50), Quaternion.identity);
+            GameObject.Find("rightWallSegment(Clone)").name = "Segment (" + position  + ")";
+        }
+
+        else if (type == 2)
+        {
+            Instantiate(middleWallSegment, new Vector3 (0, 0, farthestPlaced * 50 + 50), Quaternion.identity);
+            GameObject.Find("middleWallSegment(Clone)").name = "Segment (" + position  + ")";
+        }
+
+        else if (type == 3)
+        {
+            Instantiate(leftWallSegment, new Vector3 (0, 0, farthestPlaced * 50 + 50), Quaternion.identity);
+            GameObject.Find("leftWallSegment(Clone)").name = "Segment (" + position  + ")";
+        }
+    }   
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        farthestPlaced++;
+        placeSegment(farthestPlaced);
+        farthestPlaced++;
+        placeSegment(farthestPlaced);
     }
 
     // Update is called once per frame
@@ -22,14 +52,13 @@ public class infinite_road : MonoBehaviour
     {
         Debug.Log(transform.position.z);
 
-        if ((transform.position.z) >= (currentNum * 100 + 102))
+        if ((transform.position.z) >= (currentNum * 50 + 102))
         {   
             currentNum++;
             Destroy(current);
 
             farthestPlaced++;
-            Instantiate(blank_segment, new Vector3 (0, 0, farthestPlaced * 100 + 50), Quaternion.identity);
-            GameObject.Find("emptySegment(Clone)").name = "Segment (" + farthestPlaced  + ")";
+            placeSegment(farthestPlaced);
 
             current = GameObject.Find("Segment (" + currentNum + ")");
         }
