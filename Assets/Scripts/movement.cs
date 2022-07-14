c4e4988e113d41e4c5abf6255172616648772668
Harry <bamdad.kho@gmail.com>
2022-07-14 11:51:33 +0300

    none functioning input validation

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
-    public float speed = 2;
-
-    public GameObject self;
+    public float speed = 5;
 
     void Start()
     {
@@ -16,9 +14,16 @@ public class movement : MonoBehaviour
     void FixedUpdate()
     {
         if (GameObject.Find("Cam").GetComponent<camera_movement>().playerIsAlive)
-        {   
-            Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, 5);
-            self.GetComponent<Rigidbody>().MovePosition(transform.position + m_Input * Time.deltaTime * 15);
+        {               
+            Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+            Vector3 newPos = transform.position + new Vector3(0, 0, speed);
+
+            if(newPos.x > -9 && newPos.x < 9)
+            {
+                newPos += m_Input * speed * Time.deltaTime;
+            }
+
+            this.GetComponent<Rigidbody>().MovePosition(newPos);
         }
     }
 }
