f916472565c4884d5d2f806461e794d93971b6b2
Harry <bamdad.kho@gmail.com>
2022-07-19 16:33:57 +0300

    very slow movement

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -16,14 +16,18 @@ public class movement : MonoBehaviour
         if (GameObject.Find("Cam").GetComponent<camera_movement>().playerIsAlive)
         {               
             Vector3 m_Input = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-            Vector3 newPos = transform.position + new Vector3(0, 0, speed);
+    
+            Vector3 forward = new Vector3(0, 0, speed/4);
+            Vector3 horizontal = (m_Input * Time.deltaTime * speed) + transform.position;
 
-            if(newPos.x > -9 && newPos.x < 9)
+            if(horizontal.x > -9 && horizontal.x < 9)
             {
-                newPos += m_Input * speed * Time.deltaTime;
+                this.GetComponent<Rigidbody>().MovePosition(forward + horizontal);
             }
-
-            this.GetComponent<Rigidbody>().MovePosition(newPos);
+            else
+            {
+                this.GetComponent<Rigidbody>().MovePosition(forward * Time.deltaTime);
+            }
         }
     }
 }
